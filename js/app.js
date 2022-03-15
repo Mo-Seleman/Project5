c7de6f6fbc316b71b4e350724441ab37e76b6a1c
Mo-Seleman <92472515+Mo-Seleman@users.noreply.github.com>
2022-03-15 21:24:48 +0000

    getAttribute() and Looping

--- js/app.js
+++ js/app.js
@@ -3,3 +3,34 @@
 
 baguetteBox.run('.gridContainer');
 
+// /* KeyUp Function Linked To Search Bar */
+// const search = document.getElementById("search");
+// search.addEventListener("keyup", (e) => {
+// console.log(e.target.value.toLowerCase());
+// });
+
+// // Target All Anchor Elements
+// const anchors = document.querySelectorAll('a');
+
+// // Looping and Logging...
+// for (let i = 0; i < anchors.length; i++) {
+
+// }
+
+// // getAttribute 
+// const element = document.getElementById("search"); 
+// let text = element.getAttribute("data-caption");
+
+/* KeyUp Function Linked To Search Bar */
+const search = document.getElementById("search");
+      search.addEventListener("keyup", (e) => {
+      console.log(e.target.value.toLowerCase());
+
+const anchors = document.querySelectorAll('a'); // Target All Anchor Elements
+
+for (let i = 0; i < anchors.length; i++) { // Looping and Logging...
+
+const captions = anchors[i].getAttribute("data-caption"); // getAttribute 
+      console.log(captions)
+  }
+});
