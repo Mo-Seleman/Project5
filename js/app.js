4237e6fe52bbfac5f47d47d89e9015f496a6c844
Mo-Seleman <92472515+Mo-Seleman@users.noreply.github.com>
2022-03-17 23:56:20 +0000

    Search Bar Complete!

--- js/app.js
+++ js/app.js
@@ -1,6 +1,3 @@
-/*Providied searchFilter Plug In  */
-// const search = new Filter('search', 'data-caption');
-
 baguetteBox.run('.gridContainer');
 
 // (1) Get Input Element 
@@ -8,21 +5,20 @@ let inputLog = document.getElementById("search");
 // (2) Add Event Listener (KeyUp)
 inputLog.addEventListener('keyup', filterNames);
 
-
 function filterNames() {
 // (3)Get Value of Input
 filterValue = document.getElementById("search").value.toLowerCase()
-};
-//Get Captions
-const anchors = document.querySelectorAll('a');
 
+// (4)Get Captions
+const anchors = document.querySelectorAll('a');
 
 for (let i = 0; i < anchors.length; i++){
 let captions = anchors[i].getAttribute("data-caption").toLowerCase();
-let filter =  captions.includes(filterValue);
-}
+let filter =  captions.includes(filterValue);//Includes() Returns True or False
 
 if (filter === true) {
-    display: "block";
-} else {display: "none";
+anchors[i].style.display = 'block';
+} else {anchors[i].style.display = 'none';
 };
+ };
+  };
